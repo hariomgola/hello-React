46bd8ec24e99364f01764393b520b7965f4e3110
Hariom <harim987316@gmail.com>
2022-03-15 13:42:04 +0530

    Adding React files

---
+++ application Notes/notes.js
@@ -0,0 +1,28 @@
+/**
+
+ # React
+   - React is a javascript library to build user interfaces.
+   - Reactjs is a client side javascript library.
+
+
+
+ */
+
+// React Basic
+/**
+ # React Component
+   - Why components
+     - Reusability - Don't repeat yourself
+     - Separation of concerns - Make small saperate unit for easy access
+
+ # React Components
+   - HTML, CSS and JavaScript combine to make the react component
+   - React allows you to create re usable and reactive components consisting of 
+    HTML    CSS   JavaScript 
+     |       |        |
+     |_______+________|
+             |
+           React
+ # Create react Project
+   - npm create-react-app hello-react
+ */
