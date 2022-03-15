9b99dcb61e4c724d42067fd6edfe438721263e28
Hariom <hariom987316@gmail.com>
2022-03-15 22:41:17 +0530

    Adding notes and component

--- application Notes/notes.js
+++ application Notes/notes.js
@@ -1,11 +1,7 @@
 /**
-
  # React
    - React is a javascript library to build user interfaces.
    - Reactjs is a client side javascript library.
-
-
-
  */
 
 // React Basic
@@ -18,12 +14,49 @@
  # React Components
    - HTML, CSS and JavaScript combine to make the react component
    - React allows you to create re usable and reactive components consisting of 
-    HTML    CSS   JavaScript 
-     |       |        |
-     |_______+________|
-             |
-           React
+                      HTML    CSS   JavaScript 
+                      |       |        |
+                      |_______+________|
+                              |
+                            React
  # Create react Project
    - npm create-react-app hello-react
-   
+   - cd hello-react
+   - npm start to start the application
+
+  # JSX Code
+   - JSX stands for JavaScript XML
+
+  # How React Works
+
+  # Component in React
+    - A component in react is just a JavaScript function.
+    - A special kind of function which rerturns the JSX code.
+
+  # Using custom created component in other compoenent.
+    - Define the compoenent and export that component in other module.
+    - When using the component in html remember that 
+                  the lower case always used with inbuild tags i.e., div,h1,h2
+                  the Upper case always used with the user defined tags i.e., ExpenseItem,Expense
+
+  # Use React Component
+    - Create a functional component.
+    - return some jsx from the component
+    - import in the component which it is going to render or use.
+    - then use it in the component jsx
+    - Remeber that react compoenent only return one root element per return statement.
+
+  # Adding styling
+    - Create a saperate css class with the same name as the component with .css extension.
+    - import that css file into the js component and use the className to assign the class.
+
+  # Outputting Dynamic data
+    - React give us the special thing about the {} to import dynamic data inside it.
+    - <h1>{data}</h1> // let data = 'Hari'
+    - Here while in runtime the data variable will be replaced with Hari in runtime
+
+  # Outputting Dynamic data - Props
+    - Props simply stands for properties. we can set properties of our own custom components.
+    - simply pass the value as the argument of the input component and then receive it in the form of object from props.
+
  */
--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+// importing other component
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Monitor",
+      amount: 10000,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "Intel-Nuc",
+      amount: 50000,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e3",
+      title: "Iphone",
+      amount: 80000,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e4",
+      title: "MacBook",
+      amount: 135000,
+      date: new Date(2020, 7, 14),
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>React Expense Tracker</h2>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
     </div>
   );
 }
